a6d85776c0103a8ae8bb66c5b08c2818e4da784f
WillMash1 <wmashhadi@Gmail.com>
2022-02-07 19:22:00 +0000

    Committing the entire project

--- index.js
+++ index.js
@@ -10,11 +10,50 @@ const cuisineMealThreeTitle = document.querySelector('#cuisineTitleThree');
 const mealTwo = document.querySelector('#mealTwo');
 const mealThree = document.querySelector('#mealThree');
 
-///////////////////////////////////////////////
+
+
+//Setting up the splash screen
+
+let intro = document.querySelector('.intro');
+let logo = document.querySelector('.logo-header');
+let logoSpan = document.querySelectorAll('.logo')
+
+window.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        logoSpan.forEach((span, idx) => {
+            setTimeout(()=> {
+                span.classList.add('active');
+            }, (idx + 1) * 400)
+        });
+
+
+        setTimeout(()=> {
+            logoSpan.forEach((span, idx) => {
+                setTimeout(()=> {
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+                }, 50)
+            })
+        },2000 );
+
+        setTimeout(()=> {
+             intro.style.top = '-100vh';
+        }, 2300);
+    })
+})
+
+
+
+
+
+
+
+
+
+
 //use mountEnter and MouseLeave event Listeners
 //On mouseEnter call the addText function
 //On mouseLeave call a removeText function
-///////////////////////////////////////////////
 const addText = function(title, mealNum) {
   title.classList.remove(`imageContainer-meal${mealNum}`);
   title.classList.add('cuisine-text-appear');
@@ -24,58 +63,96 @@ const removeText = function(title, mealNum) {
   title.classList.remove('cuisine-text-appear');
 }
 
-///////////////////////////////////////////////
-//Roll over effect for the Cuise Section Images
-///////////////////////////////////////////////
-cuisineMealOneContainer.addEventListener('mouseenter', () => {
-  mealOne.classList.remove('addBlur');
-  addText(cuisineMealOneTitle, 'One');
-  mealTwo.classList.add('addBlur');
-  mealThree.classList.add('addBlur');
-})
-cuisineMealOneContainer.addEventListener('mouseleave', () => {
+//Roll over effect for the Cuisine Section Images
+
+const cuisineRollOverEnter = function(otherMealOne, otherMealTwo) {
+  otherMealOne.classList.add('addBlur');
+  otherMealTwo.classList.add('addBlur');
+}
+const cuisineRollOverLeave = function() {
   removeText(cuisineMealOneTitle,'One');
-  mealTwo.classList.remove('addBlur');
-  mealThree.classList.remove('addBlur');
-})
-cuisineMealTwoContainer.addEventListener('mouseenter', () => {
-  addText(cuisineMealTwoTitle, 'Two');
-  mealOne.classList.add('addBlur');
-  mealThree.classList.add('addBlur');
-})
-cuisineMealTwoContainer.addEventListener('mouseleave', () => {
   removeText(cuisineMealTwoTitle, 'Two');
-  mealOne.classList.remove('addBlur');
-  mealThree.classList.remove('addBlur');
-})
-cuisineMealThreeContainer.addEventListener('mouseenter', () => {
-  addText(cuisineMealThreeTitle, 'Three');
-  mealOne.classList.add('addBlur');
-  mealTwo.classList.add('addBlur');
-})
-cuisineMealThreeContainer.addEventListener('mouseleave', () => {
   removeText(cuisineMealThreeTitle, 'Three');
   mealOne.classList.remove('addBlur');
   mealTwo.classList.remove('addBlur');
+  mealThree.classList.remove('addBlur');
+}
+
+cuisineMealOneContainer.addEventListener('mouseenter', () => {
+  addText(cuisineMealOneTitle, 'One');
+  cuisineRollOverEnter(mealTwo,mealThree);
 })
 
-//////////////////////////////////////////////////////
-//Making the hamburger menu for the navBar functional
-/////////////////////////////////////////////////////
-hamburger.addEventListener('click', () => {
-    console.log('click')
-    title.classList.toggle('show');
-    navUl.classList.toggle('show');
-    
-    // navUl.classList.toggle('navbar-fade-down');
+cuisineMealTwoContainer.addEventListener('mouseenter', () => {
+  addText(cuisineMealTwoTitle, 'Two');
+  cuisineRollOverEnter(mealOne,mealThree);
 })
-///////////////////////////////////////////////
-//Making the text fadeIn on scroll
-///////////////////////////////////////////////
+
+
+cuisineMealThreeContainer.addEventListener('mouseenter', () => {
+  addText(cuisineMealThreeTitle, 'Three');
+  cuisineRollOverEnter(mealOne,mealTwo);
+
+})
+
+cuisineMealOneContainer.addEventListener('mouseleave', () => {
+  cuisineRollOverLeave();
+})
+cuisineMealTwoContainer.addEventListener('mouseleave', () => {
+  cuisineRollOverLeave();
+})
+cuisineMealThreeContainer.addEventListener('mouseleave', () => {
+  cuisineRollOverLeave();
+})
+
+
+
+
+//Making the hamburger menu for the navBar functional
+sideMenuOpen = false;
+
+hamburger.addEventListener('click', () => {
+    if(sideMenuOpen){
+      closeSlideMenu()
+    } else {
+      console.log(screen.height)
+      if(screen.width <= 440) {
+        document.querySelector('#side-menu').style.width = '50vw';
+      sideMenuOpen = !sideMenuOpen;
+      } else {
+        document.querySelector('#side-menu').style.width = '30vw';
+        sideMenuOpen = !sideMenuOpen;
+      }
+     
+    }
+})
+
+function closeSlideMenu(){
+  sideMenuOpen = !sideMenuOpen;
+  document.querySelector('#side-menu').style.width = '0';
+  
+}
+
+const root = document.querySelector('#side-menu');
+const navSection = document.querySelector('.navbar-section');
+
+document.addEventListener('click', event => {
+  console.log('this should be working')
+  if (!root.contains(event.target) && !navSection.contains(event.target) ){
+    document.querySelector('#side-menu').style.width = '0';
+    sideMenuOpen = false;
+  }
+  })
+
+
+
+
+//Making the text fadeIn on scroll for the info section
+
 const fadeText = function() {
     console.log('hello');
-    var elements;
-    var windowHeight;
+    let elements;
+    let windowHeight;
   
     function init() {
       elements = document.querySelectorAll('.hidden');
@@ -83,11 +160,12 @@ const fadeText = function() {
     }
   
     function checkPosition() {
-      for (var i = 0; i < elements.length; i++) {
-        var element = elements[i];
-        var positionFromTop = elements[i].getBoundingClientRect().top;
+      for (let i = 0; i < elements.length; i++) {
+        let element = elements[i];
+        let positionFromTop = elements[i].getBoundingClientRect().top;
         console.log(positionFromTop);
-        if (positionFromTop - windowHeight <= 0) {
+        console.log(windowHeight)
+        if (positionFromTop - windowHeight <= -250) {
           element.classList.add('text-fade-in');
           element.classList.remove('hidden');
         }
@@ -103,12 +181,6 @@ const fadeText = function() {
   };
 
  fadeText();
-
- ///////////////////////////////////////////////
-//Make the hamburger menu slide down as animation
-//Right now the title 'Risotteria Plymouth jumps down whenever the hamburger menu is opened
-//Fix it so that the title slides down smoothly in time with the hamburger menu sliding down as well, then slides back up when the hamburger menu is closed
-///////////////////////////////////////////////
 
 
 
@@ -143,7 +215,7 @@ const cuisineSectionTitle = document.querySelector('#cuisineSectionTitle');
         carousel.startSlide();    
     })
     mealThree.addEventListener('click', e => {    
-        cuisineSectionTitle.classList.add('hideText');    
+        cuisineSectionTitle.classList.add('hideText'); 
         lightbox.classList.add('active');            
         const carousel = new Carousel(0);
         lightbox.innerHTML = carousel.setHTML("imgs/lasagna.jpg","imgs/lasagna2.jpg", "imgs/lasagna3.jpg" );
